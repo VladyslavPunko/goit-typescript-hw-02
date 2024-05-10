0878585b8ede4dd4aad17ab717f70aae1a555486
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-05-10 22:29:20 +0200

    app

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,22 +3,37 @@ import "./App.css";
 import SearchBar from "./SearchBar/SearchBar";
 import { fetchPhotosByInput } from "../photos-api";
 import ImageGallery from "./ImageGallery/ImageGallery";
-import Loader from "./Loader//Loader";
+import Loader from "./Loader/Loader";
 import ErrorMessage from "./ErrorMessage/ErrorMessage";
 import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
 import ImageModal from "./ImageModal/ImageModal";
 
-function App() {
-  const [inputSearch, setInputSearch] = useState("");
-  const [photos, setPhotos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [page, setPage] = useState(false);
-  const [showBtn, setShowBtn] = useState(false);
-  const [imageSrc, setImageSrc] = useState(null);
-  const [description, setDescription] = useState(null);
+export interface Photo {
+  url: string;
+  urls: {
+    small: string;
+    regular: string;
+  };
+  alt_description: string;
+  urlModal: string;
+}
 
-  useEffect(() => {
+export interface FetchPhotoResponse {
+  total_pages: number;
+  results: Photo[];
+}
+
+function App() {
+  const [inputSearch, setInputSearch] = useState<string>("");
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [showBtn, setShowBtn] = useState<boolean>(false);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+  const [description, setDescription] = useState<string | null>("");
+
+  useEffect((): void | (() => void) => {
     if (!inputSearch) return;
 
     async function fetchPhotos() {
@@ -39,24 +54,25 @@ function App() {
     fetchPhotos();
   }, [inputSearch, page]);
 
-  const onSubmit = (inputSearch) => {
+  const onSubmit = (inputSearch: string): void => {
     setInputSearch(inputSearch);
     setPhotos([]);
     setPage(1);
     setShowBtn(false);
   };
 
-  const onClickButton = () => {
+  const onClickButton = (): void => {
     setPage((prevPage) => prevPage + 1);
   };
 
-  const openModal = (urlModal, description) => {
+  const openModal = (urlModal: string, description: string): void => {
     setImageSrc(urlModal);
     setDescription(description);
   };
 
-  const closeModal = () => {
+  const closeModal = (): void => {
     setImageSrc(null);
+    setDescription(null);
   };
 
   return (
--- src/Components/ErrorMessage/ErrorMessage.tsx
+++ src/Components/ErrorMessage/ErrorMessage.tsx
@@ -1,6 +1,7 @@
 import css from "./ErrorMessage.module.css";
+import { FC } from "react";
 
-const ErrorMessage = () => {
+const ErrorMessage: FC = () => {
   return (
     <div className={css.wrap}>
       <p className={css.text}>
