c1ed4295d92b09250296fd76b5ade9727e7585fc
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-05-10 21:39:32 +0200

    start

--- src/Components/ErrorMessage/ErrorMessage.jsx
+++ src/Components/ErrorMessage/ErrorMessage.jsx
@@ -1,13 +1,13 @@
-
-import css from "./ErrorMessage.module.css"
-import imge from "../componentst/"
+import css from "./ErrorMessage.module.css";
 
 const ErrorMessage = () => {
-  return (<div className={css.wrap}>    
-    <p className={css.text}>Woooops... Something went wrong. Please reload the page</p>
-   </div>
+  return (
+    <div className={css.wrap}>
+      <p className={css.text}>
+        Woooops... Something went wrong. Please reload the page
+      </p>
+    </div>
+  );
+};
 
-  )
-}
-
-export default ErrorMessage
+export default ErrorMessage;
