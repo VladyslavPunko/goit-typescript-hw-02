a7f045d3a99b179720e760cdf5b2dcee59f051d3
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-05-11 09:53:56 +0200

    serch bar

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,41 +1,46 @@
 import { BsSearch } from "react-icons/bs";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 
-import css from "./SearchBar.module.css"
+import css from "./SearchBar.module.css";
+import { FC, FormEvent } from "react";
 
-const SearchBar = ({ onSubmit }) => {
-
-const handleOnSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const inputSearch = form.elements.search.value;
-    if (inputSearch.trim() === "") {
-        toast.error("Please enter search term!");
-        return
-    }
-              
-    onSubmit(inputSearch); 
-  }
-    
-
-
-
-  return (<header className={css.headerSearch}>
-  <form className={css.fomrSearch} onSubmit={handleOnSubmit}> 
-    <input className={css.inputSearch} 
-      type="text"
-      autoComplete="off"
-      autoFocus
-      placeholder="Search images and photos"
-      name="search"
-    />
-          <button type="submit" className={css.btn}><BsSearch /></button>
-          <Toaster
-  position="top-right"
-  reverseOrder={false}
-/>
-  </form>
-</header>)
+interface SearchBarProps {
+  onSubmit: (inputSearch: string) => void;
 }
 
-export default SearchBar
+const SearchBar: FC<SearchBarProps> = ({ onSubmit }) => {
+  const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const inputSearch = (
+      form.querySelector<HTMLInputElement>('input[name="search"]') || {}
+    ).value;
+    if (!inputSearch || inputSearch.trim() === "") {
+      toast.error("Please enter search term!");
+      return;
+    }
+
+    onSubmit(inputSearch);
+  };
+
+  return (
+    <header className={css.headerSearch}>
+      <form className={css.fomrSearch} onSubmit={handleOnSubmit}>
+        <input
+          className={css.inputSearch}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="search"
+        />
+        <button type="submit" className={css.btn}>
+          <BsSearch />
+        </button>
+        <Toaster position="top-right" reverseOrder={false} />
+      </form>
+    </header>
+  );
+};
+
+export default SearchBar;
