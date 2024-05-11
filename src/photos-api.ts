37c50187bf18d389b84833ec4f9c6731303a6247
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-05-11 08:25:32 +0200

    api

---
+++ src/photos-api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+let perPage = 15;
+
+export async function fetchPhotosByInput<T>(
+  inputSearch: HTMLInputElement,
+  page: number
+): Promise<T> {
+  const ACCESS = "oyaKjse9erYpC8_merX5E1mvW6IUZrjoER6jMTgJ_Rs";
+  const url = `https://api.unsplash.com/search/photos?query=${inputSearch}&client_id=${ACCESS}`;
+  const respons = await axios.get<T>(url, {
+    params: {
+      per_page: perPage,
+      page: page,
+    },
+  });
+  const photos = respons.data;
+
+  return photos;
+}
